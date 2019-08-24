1551c50f0f7534de4b2eb7a10d397e6685de8f6c
Dariusz Okonski <darek200180@gmail.com>
2019-08-25 00:16:36 +0100

    Updates

---
+++ precache-manifest.330f4ae58055eb4934a0aa03b8b73923.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e5b074d055d35cc35643df998ea0d1",
+    "url": "/react-weather-app-websamuraj/index.html"
+  },
+  {
+    "revision": "ba2f2f1f75edeb8dbf83",
+    "url": "/react-weather-app-websamuraj/static/css/main.8cd20151.chunk.css"
+  },
+  {
+    "revision": "074120350bf4ccd436a1",
+    "url": "/react-weather-app-websamuraj/static/js/2.25f17baa.chunk.js"
+  },
+  {
+    "revision": "ba2f2f1f75edeb8dbf83",
+    "url": "/react-weather-app-websamuraj/static/js/main.5c5898e0.chunk.js"
+  },
+  {
+    "revision": "40eafd49766d280ae580",
+    "url": "/react-weather-app-websamuraj/static/js/runtime~main.8d181274.js"
+  }
+]);
